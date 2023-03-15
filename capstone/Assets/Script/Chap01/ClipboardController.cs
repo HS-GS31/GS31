81269b0fb1c5adc11c9999c1414ae06b87ceec4a
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-03-15 19:03:35 +0900

    노트 5초 후에 사라지고 카트 등장

--- capstone/Assets/Script/Chap01/ClipboardController.cs
+++ capstone/Assets/Script/Chap01/ClipboardController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ClipboardController : MonoBehaviour
 {
+    public GameObject clipboard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(clipboard, 5f); // 화면 앞에 보이던 클립보드 삭제
+        Invoke("ShowShoppingCart", 5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    // 쇼핑카트 보여지게 하기
+    void ShowShoppingCart()
     {
-        
+        Debug.Log("Show ShoppingCart");
+        GameObject.Find("Main Camera").transform.Find("shopping cart").gameObject.SetActive(true);
     }
 }
