65de3b2e48cc5dc224dde512108a7be011abbe7c
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2023-03-28 17:01:30 +0900

    메뉴 스크립트 값 조정 커밋
    메뉴 스크립트 값 조정 커밋

--- capstone/Assets/Script/Chap03/MenuController.cs
+++ capstone/Assets/Script/Chap03/MenuController.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     GameObject customerManager;
+    GameObject[] ingredients;
+    int top;
     private void Start()
     {
         customerManager = GameObject.Find("CustomerManager");
+        top = -1;
     }
 
-    private void OnTriggerStay(Collider other)
+    public void push(GameObject ingredient)
     {
-        
+        top++;
+        ingredients[top] = ingredient;
+        Debug.Log(ingredient + "가 들어왔다.");
+        Debug.Log("현재 꼬치 상태 " + ingredients);
+    }
+
+    public void pop(GameObject ingredient)
+    {
+        Debug.Log(ingredient + "가 빠졌다.");
+        ingredients[top] = null;
+        top--;
+        Debug.Log("현재 꼬치 상태 " + ingredients);
     }
 }
