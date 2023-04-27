0e23a6717b90f8f832d393fb71c87972a3eb23c5
gestgest <87427692+gestgest@users.noreply.github.com>
2023-04-28 02:29:14 +0900

    씬 이름 통일

--- capstone/Assets/Script/Chap02/GameManager.cs
+++ capstone/Assets/Script/Chap02/GameManager.cs
@@ -8,6 +8,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public void GoScene3()
     {
-        SceneManager.LoadScene("Chapter03Scene");
+        SceneManager.LoadScene("Chap03");
     }
 }
